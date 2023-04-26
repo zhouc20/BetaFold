1f9e08aa1ea66936aa2066e40e139b219aa1c6e4
zhouc20 <77579455+zhouc20@users.noreply.github.com>
2023-04-26 14:00:16 +0800

    Encoders
    Encoders including positional encoding at graph level

--- Encoder.py
+++ Encoder.py
@@ -10,3 +10,24 @@ class Amino_Acid(nn.Module):
 
     def forward(self, x):
         return self.encoder(x)
+
+
+class Atom_encoder(nn.Module):
+
+    def __init__(self, hid_dim):
+        super(Atom_encoder, self).__init__()
+
+        self.encoder = nn.Embedding(100, hid_dim)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Bond_encoder(nn.Module):
+    def __init__(self, hid_dim):
+        super(Bond_encoder, self).__init__()
+
+        self.encoder = nn.Embedding(100, hid_dim)
+
+    def forward(self, x):
+        return self.encoder(x)
