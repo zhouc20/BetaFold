86b1a9a78b7ae16ccff26869c945ba982a64ccff
wangrun <wangrun20@mails.tsinghua.edu.cn>
2023-06-07 11:28:26 +0800

    add evaluation and save checkpoint

--- train_struct.py
+++ train_struct.py
@@ -26,7 +26,8 @@ def main():
     print(f'{n_parameters} parameters')
     optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, network.parameters()), lr=2e-4, weight_decay=1e-8)
     criterion = torch.nn.L1Loss()
-    for epoch in range(max_epochs):
+    for epoch in range(1, max_epochs + 1):
+        network.train()
         with tqdm(desc=f'Epoch {epoch}/{max_epochs}', total=len(train_set.structures), unit='proteins') as pbar:
             for i in range(len(train_set)):
                 data = train_set[i]
@@ -41,7 +42,19 @@ def main():
                 with torch.no_grad():
                     pbar.set_postfix({'loss': f'{criterion(data["Tm"].to(device), pred * task_std + task_mean).item():.2f}'})
                 pbar.update(train_set.batch_size)
-                
+        test_loss = []
+        network.eval()
+        with torch.no_grad():
+            for i in range(len(test_set)):
+                data = test_set[i]
+                pos = data['pos'].to(device)
+                batch = data['batch'].to(device)
+                node_atom = data['node_atom'].to(device)
+                pred = network(f_in=None, pos=pos, batch=batch, node_atom=node_atom)
+                loss = criterion(data["Tm"].to(device), pred * task_std + task_mean).item()
+                test_loss.append(loss.item())
+        print(f'test avg MAE={sum(test_loss) / len(test_loss)}')
+        torch.save(network.state_dict(), f'./checkpoints/epoch_{epoch}.pth')
                 
         
 
